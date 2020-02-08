3a6a8db1c691307b974a4ff985a907173d88aaa0
TymoteuszChatys <tymoteuszchatys@gmail.com>
2020-02-08 19:44:28 +0000

    Added: Program now exits after wrong file name

--- Assessment2/Ass2.cpp
+++ Assessment2/Ass2.cpp
@@ -13,14 +13,29 @@
 
 double mean(double* data,int data_points);
 double standard_deviation(double* data, int data_points, double mean);
-std::tuple <double*, int> file_processing();
+std::tuple <double*, int> file_processing(std::string file_name);
 
 int main()
+// Main part of the program. Calls all the needed functions.
 {
+  std::string file_name;
+  std::fstream file;         // a file stream for reading or writing
+  std::string units = "10^-19 Coulomb";
   int count{};
   double* data = NULL;
-  std::string units = "10^-19 Coulomb";
-  std::tie(data,count) = file_processing();
+
+  std::cout << "Enter the filename: ";
+  std::cin >> file_name;
+  file.open(file_name);
+  if(! file.good ()) {
+    std::cerr << "Error: file with the name " << file_name << " could not be opened" << std::endl;
+    std::cout << "Input anything to exit " << std::endl; 
+    std::cin.ignore();  
+    std::cin.get(); 
+    return 0;
+  }
+  file.close();
+  std::tie(data,count) = file_processing(file_name);
   double data_mean = mean(data,count);
   double data_standard_deviation = standard_deviation(data,count,data_mean);
   double data_standard_error = data_standard_deviation/sqrt(count);
@@ -29,27 +44,18 @@ int main()
   std::cout << "Standard Error in the Mean: " << data_standard_error << std::endl;
   std::cout << "All in units of " << units << std::endl;
   delete[] data;
+  std::cout << " " << std::endl << " " << std::endl << "Input anything to exit " << std::endl; 
+  std::cin.ignore();  
+  std::cin.get(); 
   return 0;
 }
 
-std::tuple <double*, int> file_processing(){
-    // Read data from file, ignoring any additional bad data
-  int i{1};
-  std::string file_name;
-  std::ifstream input_file;  // a file stream for reading only
-  std::ofstream output_file; // a file stream for writing only
-  std::fstream file;         // a file stream for reading or writing
-  do{
-    std::cout << "Enter the filename: ";
-    std::cin >> file_name;
-    file.open(file_name);
-    if(! file.good ()) {
-      std::cerr << "Error: file with the name " << file_name << " could not be opened" << std::endl;    }
-    else{
-      i = 0;
-    }
-  }while(i==1);
-
+std::tuple <double*, int> file_processing(std::string file_name)
+//This function reads the file, finds the amount of data points in the file 
+//Stores the data points into an array and returns the array and number of data points.
+{  
+  std::fstream file; 
+  file.open(file_name);
   int number_of_data_points{};
   while (!file.eof()) {
     std::string a_line;
