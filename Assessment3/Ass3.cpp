cb7761617e35ecc6e30f5cef55bf59ee98328202
TymoteuszChatys <tymoteuszchatys@gmail.com>
2020-02-19 16:01:55 +0000

    added comments

--- Assessment3/Ass3.cpp
+++ Assessment3/Ass3.cpp
@@ -31,6 +31,7 @@ int main(void)
 
     choice = options(); //Asks user how he wants to use the program
     if(choice == "file"){
+        //Looks at the file and inputs the two forms of data into vectors.
         std::tie(course_code, course_name, end) = file();
         if(end == "end"){
             return 0;
@@ -39,16 +40,13 @@ int main(void)
     else if(choice == "manual"){
         //course_code, course_name = manual();
     }    
-    //    } while(not_finished);
 
+    //Pairs the two vectors (course code and corse name) together to allow manipulation later.  
     std::vector<std::pair<std::string,std::string>> pair;
     for (int i{}; i<course_code.size(); i++){
         pair.push_back(make_pair(course_code[i],course_name[i]));
     }
     
-
-    //Creates a vector of output strings in the correct format ready for output.
-
     std::cout << "What courses would you like to display?" << std::endl
     << "(1) all courses as entered" << std::endl
     << "(2) by title" << std::endl
@@ -57,14 +55,13 @@ int main(void)
     
     int option_picked;
     checkInteger(option_picked,1, 4, "Answer value 1-4 : ", "Invalid input; please make sure you are inputting an integer", "Invalid input; integer must be between 1 and 4");
-    if(option_picked == 2){
-        //Sort by title
+    if(option_picked == 2){ //Sort by title
     }
-    else if(option_picked == 3){
-        //Sort by course code
+    else if(option_picked == 3){ //Sort by course code
         sort(pair.begin(), pair.end()); 
     }
 
+    //Creates a vector of output strings in the correct format ready for output.
     std::vector<std::string> output_string;
     std::string  course_string;
     for (int i=0; i<course_code.size(); i++) { 
@@ -72,30 +69,34 @@ int main(void)
         output_string.push_back(course_string);
     } 
 
+    //Outputs the courses needed using iterators.
     for (auto iterator = output_string.begin(); iterator != output_string.end(); iterator++) {
             std::cout << *iterator << std::endl;
     }
 }
 
 std::string options(){
+    //Function is used to determine the way the user would like to use the program
     std::string option;
-    // Gather list of courses and their codes from user,
     std::cout << "How would you like to input courses? " << std::endl;
     std::cout << "1 - from file " << std::endl << "2 - enter manually " << std::endl;
     std::cin >> option;
     while (!(option == "1" || option == "2")){
+        //If user inputs are out of our range of options
         std::cout << "Invalid input. Please enter one of the options. To view the options again enter 0 " << std::endl;
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::cin >> option;
         if(option == "0"){
+            //To remind the user of the options available
             std::cout << "How would you like to input courses? " << std::endl;
             std::cout << "1 - from file " << std::endl << "2 - enter manually " << std::endl; 
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cin >> option;
         }
-    }
+    } 
+    //Now sets to the correct string that can be read and returned to the main function of the program
     if(option == "1"){
         option = "file";
     }
@@ -107,11 +108,13 @@ std::string options(){
 
 std::tuple <std::vector<std::string>,std::vector<std::string>,std::string> file()
 {
+    //Defining variables and vectors
     std::vector<std::string> course_code;
     std::vector<std::string> course_name;
     std::string file_name;
     std::string end = "continue";
 
+    //This checks for the file and whether it can be accessed by the program
     std::fstream file;       
     std::cout << "Enter the filename: ";
     std::cin >> file_name;
@@ -123,26 +126,29 @@ std::tuple <std::vector<std::string>,std::vector<std::string>,std::string> file(
         std::cin.ignore();  
         std::cin.get();
         }
+    //If it can be accessed input the data from the file to vectors
     else{
         int i{};
         while (!file.eof()) {
             std::string a_line;
             getline(file, a_line);
             std::istringstream iss(a_line);
+            //Seperates the line into many strings
             std::vector<std::string> line_element{
                 std::istream_iterator<std::string>{iss},
-                std::istream_iterator<std::string>{}
-                };
-
+                std::istream_iterator<std::string>{}};
             std::string total_second_line_element;
+            //Takes every seperate string after the first string and combines them to form the course name
             for(int n{1}; n < (line_element.size()) ;n++){
                 total_second_line_element = total_second_line_element + " " + line_element[n];
             }
+            //Checks to see if the course code is an integer, if it is, the course is entered
             if(is_number(line_element[0]) == true){
                 course_code.push_back(line_element[0]);
                 course_name.push_back(total_second_line_element);
                 i++;
             }
+            //Otherwise an error is shown.
             else{
                 std::cout << "Error: Course number " << i+1 << " in " << file_name << " has a non-integer course code. " << total_second_line_element << " has been omitted. " << std::endl;
             }
@@ -162,8 +168,9 @@ int manual(){
 }
 
 
-//Function to check wether a string is a number
+
 bool is_number(const std::string& s)
+//Function to check wether a string is a number
 {
     return !s.empty() && std::find_if(s.begin(), 
         s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
@@ -191,7 +198,7 @@ void checkInteger(int& integer_entered, int min, int max, std::string prompt, st
             if(integer_entered > max || integer_entered < min) {
               std::cout << rangefail << std::endl;
               continue;
-        }
+            }
         }
         break;
     }
