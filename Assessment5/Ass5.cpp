34905c9e45fc542ecb727e9a77583c1c8182c294
TymoteuszChatys <TymoteuszChatys@gmail.com>
2020-02-28 13:58:41 +0000

    functions

--- Assessment5/Ass5.cpp
+++ Assessment5/Ass5.cpp
@@ -9,25 +9,27 @@
 class complex
 {
   // Make function to overload operator<< a friend
-  friend std::ostream & operator<<(std::ostream &os, const complex &z);
+    friend std::ostream & operator<<(std::ostream &os, const complex &z);
 private:
-  double re,im;
+    double real,imaginary;
 public:
   // Constructors & destructor
-  complex(){re=im=0;}
-  complex(double real_part, double im_part){re=real_part; im=im_part;}
-  ~complex(){}
+    complex(){real=imaginary=0;}
+    complex(double real_part, double imaginary_part) : real{real_part}, imaginary{imaginary_part} {}
+    ~complex(){}
 
-  // Return real component
-
-  // Return imaginary component
-
+  // Get functions
+    double get_real_part() const {return real;}
+    double get_imaginary_part() const {return imaginary;}
+  // Set functions
+    void set_real_part(const double new_real) {real = new_real;}
+    void set_imaginary_part(const double new_imaginary) {imaginary = new_imaginary;}
   // Return modulus
-
+    double modulus() const {return sqrt(real*real+imaginary*imaginary);}
   // Return argument
-
-  // Return complex conjugate
-
+    double argument() const {return atan2(imaginary,real);}
+  // Return complex conjugate function (outside class)
+    complex conjugate() const;
   // Overload + operator for addition 
 
   // Overload - operator for subtraction
@@ -38,16 +40,31 @@ public:
 
 };
 
+complex complex::conjugate() const
+{
+    complex temporary(real, imaginary);
+    temporary.set_real_part(real);
+    temporary.set_imaginary_part(imaginary*-1);
+    return temporary;
+}
+
+std::ostream & operator<<(std::ostream &os, const complex &v)
+  {
+    os<<v.real<<","<<v.imaginary<<std::endl;
+    return os;
+  }
+
 // Function to overload << operator for complex numbers
 
 int main()
 {  
-  std::cout.precision(3);
+    std::cout.precision(3);
 
   // Create two complex numbers
-  complex a{3,4};
-  complex b{1,-2};
-
+    complex a{3,4};
+    complex b{1,-2};
+    std::cout << a;
+    std::cout << a.conjugate();
  // Get real and imaginary components, modulus and argument
 
   // Get conjugates
@@ -58,5 +75,5 @@ int main()
 
   // Show results of overloading arithmetic operators
 
-  return 0;
+    return 0;
 }
