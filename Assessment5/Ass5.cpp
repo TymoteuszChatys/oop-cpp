4e0acc7442fa69d3d02c81dc4af985e55bef9e73
TymoteuszChatys <TymoteuszChatys@gmail.com>
2020-02-29 14:44:44 +0000

    fixed warnings

--- Assessment5/Ass5.cpp
+++ Assessment5/Ass5.cpp
@@ -31,13 +31,34 @@ public:
   // Return complex conjugate function (outside class)
     complex conjugate() const;
   // Overload + operator for addition 
-
+    complex operator+(const complex &v) const
+    {
+    complex temp{real+v.real,imaginary+v.imaginary};
+    return temp;
+    }
   // Overload - operator for subtraction
-
+    complex operator-(const complex &v) const
+    {
+    complex temp{real-v.real,imaginary-v.imaginary};
+    return temp;
+    }
   // Overload * operator for multiplication, z1*z2
-
+    complex operator*(const complex &v) const
+    {
+    double total_real = real*v.real-imaginary*v.imaginary;
+    double total_imaginary = real*v.imaginary+v.real*imaginary;
+    complex temp{total_real, total_imaginary};
+    return temp;
+    }
   // Overload / operator for division, z1/z2
-
+    complex operator/(const complex &v) const
+    {
+    complex conjugate = v.conjugate();
+    complex numerator = operator*(conjugate);
+    double denominator = pow(v.modulus(),2);
+    complex temp{numerator.real/denominator, numerator.imaginary/denominator};
+    return temp;
+    }
 };
 
 complex complex::conjugate() const
@@ -48,32 +69,48 @@ complex complex::conjugate() const
     return temporary;
 }
 
-std::ostream & operator<<(std::ostream &os, const complex &v)
-  {
-    os<<v.real<<","<<v.imaginary<<std::endl;
-    return os;
-  }
+std::ostream & operator<<(std::ostream &out_stream, const complex &v)
+{
+    if (v.imaginary >= 0){
+      out_stream<<v.real<<"+i"<<v.imaginary;
+    }
+    else{
+      out_stream<<v.real<<"-i"<<abs(v.imaginary);
+    }
+    return out_stream;
+}
 
 // Function to overload << operator for complex numbers
+void dash(){
+  std::cout << "----------------------------------" << std::endl;
+}
 
 int main()
 {  
+    dash();
+    std::cout << "Complex numbers program ULTRA8000" << std::endl;
+    dash();
     std::cout.precision(3);
-
   // Create two complex numbers
     complex a{3,4};
     complex b{1,-2};
-    std::cout << a;
-    std::cout << a.conjugate();
- // Get real and imaginary components, modulus and argument
+    std::cout << "Pre-set example, complex numbers: " << std::endl;
+    std::cout << "   " << a <<  "     and     " << b <<std::endl;
+    dash();
+  // Get real and imaginary components, modulus and argument
 
   // Get conjugates
 
   // Get sum, difference, product and quotient of a and b
-
+    std::cout << "Results: " << std::endl;
+    std::cout << "sum " <<  a + b <<std::endl;
+    std::cout << "difference " << a - b <<std::endl;
+    std::cout << "product " << a * b <<std::endl;
+    std::cout << "quotient " << a / b <<std::endl;
+    std::cout << "conjugate of " << a << ": " << a.conjugate() << std::endl;
+    std::cout << "conjugate of " << b << ": " << b.conjugate() << std::endl;
   // Print out results, also demonstrating use of overloaded operator<<
 
   // Show results of overloading arithmetic operators
-
     return 0;
 }
