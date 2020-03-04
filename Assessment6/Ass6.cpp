2b25b614176e211a4f2fb252bc8c6e601bcdfc98
TymoteuszChatys <TymoteuszChatys@gmail.com>
2020-03-04 23:01:29 +0000

    Added assessment 6

---
+++ Assessment6/Ass6.cpp
@@ -0,0 +1,130 @@
+// PHYS 30762 Programming in C++
+// Assignment 6
+
+// A matrix class - skeleton code
+
+// You are advised to write your own code from scratch but
+// you may use this code if you do not know how to get started.
+
+// Note: for longer functions, you are advised to prototype them
+// within class and put the function code itself immediately below class. 
+
+#include<iostream>
+
+class matrix
+{
+  // Friends
+  friend ostream & operator<<(ostream &os, const matrix &mat);
+private:
+  double *matrix_data {nullptr};
+  int rows{0};
+  int columns{0};
+public:
+  // Default constructor
+  matrix()= default;
+  // Parameterized constructor
+  
+  // Copy constructor
+
+  // Move constructor
+
+  // Destructor
+
+  // Access functions
+  int get_rows() const {return rows;} // Return number of rows
+  int get_cols() const {return columns;} // Return number of columns
+  int index(int m, int n) const // Return position in array of element (m,n)
+  {
+    if(m>0 && m<=rows && n>0 && n<=columns) return (n-1)+(m-1)*columns;
+    else {std::cout<<"Error: out of range"<<endl; exit(1);}
+  }
+  double & operator()(int m, int n) {return m_data[index(m,n)];}
+  // Other access functions go here
+
+  // Other functions 
+  // Copy  Assignment operator
+
+  // Move Assignment operator
+
+  // Addition, subtraction and multiplication
+
+  // minor
+
+  // determinant
+
+};
+
+// Member functions defined outside class
+
+
+
+// Overload insertion to output stream for matrices
+ostream & operator<<(ostream &os, const matrix &mat)
+{
+  // Code goes here
+
+  return os;
+}
+
+// Main program
+
+int main()
+{
+
+  //
+  // First part of assignment: constructing and deep copying matrices
+  //
+
+  // Demonstrate default constructor
+  matrix a1;
+  std::cout<<a1;
+
+  // Parameterized constructor
+  const int m{2};
+  const int n{2};
+  matrix a2{m,n};
+  // Set values for a2 here
+
+  // Print matrix a2
+  std::cout<<a2;
+
+
+  // Deep copy by assignment: define new matrix a3 then copy from a2 to a3
+  matrix a3{m,n};
+  std::cout<<a3;
+  a3=a2;
+  std::cout<<a3;
+  // Modify contents of original matrix and show assigned matrix is unchanged here
+
+  std::cout<<a2;
+  std::cout<<a3;
+ 
+
+  // Deep copy using copy constructor 
+  matrix a4{a2};
+  std::cout<<a4;
+  // Modify contents of original matrix and show copied matrix is unchanged here
+
+  std::cout<<a2;
+  std::cout<<a4;
+
+  // Move copy construction demonstration
+
+  // Move assignment demonstration
+
+  //
+  // Second part of assignment: matrix operations
+  //
+
+  // Addition of 2 matrices
+
+  // Subtraction of 2 matrices
+
+  // Multiplication of 2 matrices
+
+
+  // Determinant
+
+  
+  return 0;
+}
