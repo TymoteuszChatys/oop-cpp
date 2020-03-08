16404017404ead865bb4572df1518e57c6b70813
TymoteuszChatys <TymoteuszChatys@gmail.com>
2020-03-08 17:45:34 +0000

    changes

--- Assessment6/Ass6.cpp
+++ Assessment6/Ass6.cpp
@@ -21,21 +21,23 @@ private:
   int columns{};
 public: 
   // Default, Parameterized, Copy, Move Constructor
-  matrix() : rows = {}, columns = {}, matrix_data = {};
-  matrix(int, int); 
-  matrix(const matrix&);
-  matrix(matrix&&); 
+  matrix();
+  matrix(int a, int b); 
+  matrix(const matrix &a_matrix) ;
+  matrix(matrix &&a_matrix); 
   // Destructor
-  ~matrix();
+  ~matrix(){std::cout<<"Destructor called" << std::endl; delete[] matrix_data;};
   // Access functions
-  int get_rows() const { // Return number of rows
+  int get_rows() const 
+  { // Return number of rows
     return rows;
   } 
-  int get_columns() const { // Return number of columns
+  int get_columns() const 
+  { // Return number of columns
     return columns;
   } 
-  int index(int m, int n) const // Return position in array of element (m,n)
-  {
+  int index(int m, int n) const 
+  { // Return position in array of element (m,n)
     if(m>0 && m<=rows && n>0 && n<=columns) {
       return (n-1)+(m-1)*columns;
     }
@@ -43,22 +45,56 @@ public:
       std::cout<<"Error: out of range"<<std::endl; exit(1);
     }
   }
-  double & operator()(int m, int n) {
+  double& operator()(int m, int n) 
+  {
     return matrix_data[index(m,n)];
   }
   //Copy and Move Assignment operators
-  matrix& operator=(const matrix&);
-  matrix& operator=(matrix &&);
+  matrix& operator=(matrix &a_matrix);
+  matrix& operator=(matrix &&a_matrix);
   //Addition, Subtraction and Multiplication overloading
-  matrix operator+(const matrix&) const;
-  matrix operator-(const matrix&) const; 
-  matrix operator*(const matrix&) const;
-
+  matrix operator+(const matrix &a_matrix) const;
+  matrix operator-(const matrix &a_matrix) const; 
+  matrix operator*(const matrix &a_matrix) const;
   //Calculation of the determinant
   matrix minor(int, int) const;
   double determinant() const;
 };
-
+//Default constructor
+matrix::matrix() : rows{2}, columns{2}
+{
+  std::cout<<"Default constructor called" << std::endl; 
+  matrix_data = new double[rows*columns]{};
+}
+//Parameterized constructor
+matrix::matrix(int row_number, int column_number) : rows{row_number} , columns{column_number}
+{
+  std::cout<<"Parameterized constructor called" << std::endl;
+  if (rows == 0 || columns == 0){
+    std::cout << "Error: 0 dimensional matrix";
+    exit(1);
+  }else{
+    matrix_data = new double[rows*columns]{};
+  }
+}
+//copy constructor
+matrix::matrix(const matrix &a_matrix) : rows{a_matrix.rows} , columns{a_matrix.columns}
+{
+  std::cout << "copy constructor" << std::endl; 
+  size_t size = rows*columns;
+  new_matrix = new double[size]{};
+  for(size_t i{}; i<size; i++;){
+    a_matrix[i] = matrix.new_matrix[i];
+  }
+}
+//Move constructor
+matrix::matrix(matrix &&a_matrix)
+{
+  std::cout << "move constructor" << std::endl;
+  rows = a_matrix.rows;
+  columns = a_matrix.columns;
+  matrix_data = a_matrix.matrix_data;
+}
 // Member functions defined outside class
 
 
@@ -79,15 +115,29 @@ int main()
   //
 
   // Demonstrate default constructor
+  matrix a1;
+  std::cout<<a1;
    // Parameterized constructor
+  const int m{2};
+  const int n{2};
+  matrix a2{m,n};
   // Set values for a2 here
-
   // Print matrix a2
+  std::cout<<a2;
   // Deep copy by assignment: define new matrix a3 then copy from a2 to a3
-
+  matrix a3{m,n};
+  std::cout<<a3;
+  a3=a2;
+  std::cout<<a3;
   // Modify contents of original matrix and show assigned matrix is unchanged here
+  std::cout<<a2;
+  std::cout<<a3;
   // Deep copy using copy constructor 
+  matrix a4{a2};
+  std::cout<<a4;
   // Modify contents of original matrix and show copied matrix is unchanged here
+  std::cout<<a2;
+  std::cout<<a4;
   // Move copy construction demonstration
 
   // Move assignment demonstration
