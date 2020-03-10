289895d123367086cff403fe75d32a9f52034260
TymoteuszChatys <TymoteuszChatys@gmail.com>
2020-03-10 00:56:26 +0000

    copy/move [part 1 finished]

--- Assessment6/Ass6.cpp
+++ Assessment6/Ass6.cpp
@@ -5,13 +5,11 @@
 /*
 Input example: 
 1 2 12 2 5 7 12 2 1
-
 Output example:
 |1 2 12|
 |2 5 7|
 |12 2 1|
 */
-
 #include<iostream>
 
 class matrix
@@ -29,7 +27,7 @@ public:
   matrix(const matrix &a_matrix) ;
   matrix(matrix &&a_matrix); 
   // Destructor
-  ~matrix(){std::cout<<"Destructor called" << std::endl; delete[] matrix_data;};
+  ~matrix(){delete[] matrix_data;};
   //Copy and Move Assignment operators
   matrix& operator=(matrix &a_matrix);
   matrix& operator=(matrix &&a_matrix);
@@ -78,7 +76,7 @@ public:
 };
 
 //Default constructor
-matrix::matrix() : rows{2}, columns{2}
+matrix::matrix() : rows{3}, columns{3}
 {
   matrix_data = new double[rows*columns]{};
 }
@@ -114,24 +112,22 @@ matrix::matrix(matrix &&a_matrix)
   a_matrix.matrix_data = nullptr;
 }
 //Copy assignment operator
-matrix & matrix::operator=(matrix &a_matrix)
+matrix &matrix::operator=(matrix &a_matrix)
 {
   if(&a_matrix == this){ //no self assignment
     return *this;
   } 
   //First need to delete the objects array
   delete[] matrix_data;
-  matrix_data = nullptr;
-  rows = {};
-  columns = {};
+  matrix_data = {};
   //Copy the size and declare new array
-  size_t size = a_matrix.rows*a_matrix.columns;
-  if(size>0){
-    matrix_data = new double[size];
+  rows = a_matrix.rows;
+  columns = a_matrix.columns;
+  size_t size = rows*columns;
+  matrix_data = new double[size];
     //Copy values into new array
-    for(size_t i{}; i<size;i++){
-      matrix_data[i] = a_matrix.matrix_data[i];
-    }
+  for(size_t i{}; i<size;i++){
+    matrix_data[i] = a_matrix.matrix_data[i];
   }
   return *this;
 }
@@ -155,15 +151,15 @@ std::ostream & operator<<(std::ostream &out_stream, const matrix &a_matrix)
   for(size_t i=1; i<a_matrix.rows+1; i++){
     for (size_t j=1; j<a_matrix.columns+1; j++){
       if(a_matrix.columns==1){ //Single column special case
-        out_stream << '|' << " " << a_matrix.matrix_data[a_matrix.index(i,j)] << " " << '|' << std::endl;
+        out_stream << '|' << a_matrix.matrix_data[a_matrix.index(i,j)] << '|' << std::endl;
       }else{
         if(j==1){
-          out_stream << '|' << " " << a_matrix.matrix_data[a_matrix.index(i,j)] << " ";
+          out_stream << '|' << a_matrix.matrix_data[a_matrix.index(i,j)] << " ";
         }else if(j==a_matrix.columns){
             if(i==a_matrix.rows){
-              out_stream << a_matrix.matrix_data[a_matrix.index(i,j)] << " " << '|'; //Last row we dont need a new line for proceeding rows. 
+              out_stream << a_matrix.matrix_data[a_matrix.index(i,j)] << '|'; //Last row we dont need a new line for proceeding rows. 
             }else{
-              out_stream << a_matrix.matrix_data[a_matrix.index(i,j)] << " " << '|' << std::endl;
+              out_stream << a_matrix.matrix_data[a_matrix.index(i,j)] << '|' << std::endl;
             }
           
         }else {
@@ -225,39 +221,68 @@ std::string dash(){
 //Main program
 int main()
 {
-  std::cout << dash() << std::endl << "Matrix calculator" << std::endl << dash() << std::endl;
-  //Demonstrate default constructor
-  matrix a1;
-  std::cout << "Default constructor: " << std::endl << a1 << std::endl << dash() << std::endl;
-  //Demonstrate parameterized constructor
-  const int m{2};
-  const int n{2};
-  matrix a2;
-  std::cin >> a2;
-  std::cout << "Parameterized constructor: " << std::endl << a2 << std::endl << dash() << std::endl; 
-  // Deep copy by assignment: define new matrix a3 then copy from a2 to a3
-  std::cout << "Deep copy by assignmnent: " << std::endl;
-  matrix a3{m,n};
-  std::cout << "Default: " << std::endl << a3 << std::endl; 
-  a3=a2;
-  std::cout << "Deep copy: " << std::endl << a3 << std::endl << dash() << std::endl; 
-  // Modify contents of original matrix and show assigned matrix is unchanged here
- // std::cout<<a2;
-  //std::cout<<a3;
-  // Deep copy using copy constructor 
- // matrix a4{a2};
-  //std::cout<<a4;
-  // Modify contents of original matrix and show copied matrix is unchanged here
-  //std::cout<<a2;
-  //std::cout<<a4;
-  // Move copy construction demonstration
-
-  // Move assignment demonstration
-
-  //
-  // Second part of assignment: matrix operations
-  //
-
+  std::cout << dash() << dash() << std::endl << "Matrix calculator" << std::endl << dash() << dash() << std::endl;
+  std::cout << "Which option would you like to choose?" << std::endl;
+  std::cout << "1 - matrix calculator" << std::endl;
+  std::cout << "2 - copy/move constructors/assignment demonstration using matrices" << std::endl << dash() << dash() << std::endl;
+  //Asks user to choose an option
+  std::string option;
+  getline(std::cin, option);
+  if(option=="2"){
+    //Demonstrate default constructor
+    matrix a1;
+    std::cout << "Default constructor (a1): " << std::endl << a1 << std::endl << dash() << std::endl;
+    //Demonstrate parameterized constructor
+    /* Make matrix a2
+    |1 2| 
+    |3 4|
+    */
+    const int m{2};
+    const int n{2};
+    matrix a2{m,n};
+    a2.set_value(1,1,1); a2.set_value(1,2,2); a2.set_value(2,1,3); a2.set_value(2,2,4);
+    std::cout << "Parameterized constructor (a2): " << std::endl << a2 << std::endl << dash() << std::endl; 
+    //Deep copy by assignment: define new matrix a3 then copy from a2 to a3
+    std::cout << "Deep copy by assignmnent: " << std::endl;
+    matrix a3{m,n};
+    std::cout << "Default (a3): " << std::endl << a3 << std::endl; 
+    a3=a2;
+    std::cout << "Deep copy (a2 to a3): " << std::endl << a3 << std::endl << dash() << std::endl;
+    //Modify contents of original matrix and show assigned matrix is unchanged here
+    std::cout << "Now modifying a2 and outputting a3: " << std::endl;
+    /* change a2 to
+    |2 2| 
+    |2 2|
+    */
+    a2.set_value(1,1,2); a2.set_value(1,2,2); a2.set_value(2,1,2); a2.set_value(2,2,2);
+    std::cout << "a2: " << std::endl << a2 << std::endl;
+    std::cout << "a3: " << std::endl << a3 << std::endl << dash() << std::endl;
+    //Deep copy using copy constructor
+    std::cout << "Deep copy using copy constructor (a2 to a4): " << std::endl; 
+    matrix a4{a2};
+    std::cout << "a4:" << std::endl << a4 << std::endl << dash() << std::endl;
+    //Modify contents of original matrix and show copied matrix is unchanged here
+    std::cout << "Now modifying a2 and outputting a4: " << std::endl; 
+    /* change a2 to
+    |5 1| 
+    |0 7|
+    */
+    a2.set_value(1,1,5); a2.set_value(1,2,1); a2.set_value(2,1,0); a2.set_value(2,2,7);
+    std::cout << "a2:" << std::endl << a2 << std::endl;
+    std::cout << "a4:" << std::endl << a4 << std::endl << dash() << std::endl;
+    //Move copy construction demonstration
+    std::cout << "a5 creation using move constructor from a2: " << std::endl;
+    matrix a5(std::move(a2));
+    std::cout << "a2:" << std::endl << a2;
+    std::cout << "a2 now empty, it has moved to a5 " << std::endl;
+    std::cout << "a5:" << std::endl << a4 << std::endl << dash() << std::endl;
+    // Move assignment demonstration
+    std::cout << "moving a5 back to a2 using move assignment: " << std::endl;
+    a2 = std::move(a5);
+    std::cout << "a2:" << std::endl << a2 << std::endl;
+    std::cout << "a5:" << std::endl << a5;
+    std::cout << "a5 now empty, has moved to a2 " << std::endl << dash() << std::endl;
+  }
   // Addition of 2 matrices
 
   // Subtraction of 2 matrices
