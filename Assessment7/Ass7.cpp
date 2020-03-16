fbadcd3885712e184a3b6e915d171e01f8a89d1c
TymoteuszChatys <TymoteuszChatys@gmail.com>
2020-03-16 23:19:34 +0000

    break lines

---
+++ Assessment7/Ass7.cpp
@@ -0,0 +1,30 @@
+//16th March 2020
+//Assignment 7
+//Visual studio code
+
+
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+#include<string>
+
+std::string dash(int i)
+{
+    std::string break_line;
+    if(i=0){
+	    break_line = "---------------------------------------";
+    }else if(i=1){
+	    break_line = "-----------------ERROR-----------------";
+    }
+	return break_line;
+}
+
+class vector
+{
+
+private:
+    
+
+
+
+}
