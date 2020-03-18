8d4a965ec66b19ea52b7a92998719f335923f254
TymoteuszChatys <TymoteuszChatys@gmail.com>
2020-03-18 20:33:24 +0000

    four-vector class

--- Assessment7/Ass7.cpp
+++ Assessment7/Ass7.cpp
@@ -25,35 +25,35 @@ std::string dash(int i)
 
 class vector
 {
-    //friends of the class
-    friend std::istream& operator>>(std::istream& in_stream, vector& a_vector);
-    friend std::ostream &operator<<(std::ostream &out_stream, const vector &a_vector);
+  //friends of the class
+  friend std::istream& operator>>(std::istream &in_stream, vector& a_vector);
+  friend std::ostream &operator<<(std::ostream &out_stream, const vector &a_vector);
 
 protected:
-    double* vector_data{nullptr};
-    size_t size{};
+  double* vector_data{nullptr};
+  size_t size{};
 
 public:
-    // Default, Parameterized, Copy, Move Constructor
-    vector();
+  // Default, Parameterized, Copy, Move Constructor
+  vector();
 	vector(size_t length);
 	vector(const vector& a_vector);
 	vector(vector&& a_vector) noexcept;
 	// Destructor
 	~vector() {delete[] vector_data;};
-    //Copy and Move Assignment operators
+  //Copy and Move Assignment operators
 	vector& operator=(vector& a_vector);
 	vector& operator=(vector&& a_vector) noexcept;
-    //overload [] operator for accessing elements of the vector
-    double& operator[](const size_t index);
-    //overload * for dot product of two vectors
-    double operator*(const vector& a_vector) const;
-    //get and set functions
-    size_t get_size() const
+  //overload [] operator for accessing elements of the vector
+  double& operator[](const size_t index);
+  //overload * for dot product of two vectors
+  double operator*(const vector& a_vector) const;
+  //get and set functions
+  size_t get_size() const
 	{
-		return size;
+    return size;
 	}
-    void set_value(const size_t& i, const double& value)
+  void set_value(const size_t& i, const double& value)
 	{//Set value i'th component of the vector
 		vector_data[i] = value;
 	}
@@ -130,11 +130,11 @@ std::ostream &operator<<(std::ostream &out_stream, const vector &a_vector){
   if(a_vector.get_size() > 0){
     out_stream << "[";
     for (size_t index{0}; index < a_vector.get_size(); index++) {
-        if (index == a_vector.get_size() - 1) {
-            out_stream << a_vector.vector_data[index] << "]";
-        }else{
-            out_stream << a_vector.vector_data[index] << ";";
-        }
+      if (index == a_vector.get_size() - 1) {
+        out_stream << a_vector.vector_data[index] << "]";
+      }else{
+        out_stream << a_vector.vector_data[index] << ";";
+      }
     }
   }else{
     out_stream << "vector is empty" << std::endl;
@@ -143,7 +143,7 @@ std::ostream &operator<<(std::ostream &out_stream, const vector &a_vector){
 }
 
 //Overload >> operator to input vectors
-std::istream& operator>>(std::istream& in_stream, vector& a_vector)
+std::istream &operator>>(std::istream &in_stream, vector &a_vector)
 {
 	/*
 	Input example:
@@ -178,105 +178,299 @@ std::istream& operator>>(std::istream& in_stream, vector& a_vector)
 
 double vector::operator*(const vector& a_vector) const
 {
-    double dot_product{0};
+    double dot_product{};
     if (size != a_vector.size) {
         std::cout << "Error: vectors must have the same size" << std::endl; exit(1);
     }
-    for (int i{0}; i < size; i++) {
+    for (size_t i{}; i < size; i++) {
     dot_product += vector_data[i] * a_vector.vector_data[i];
     }
     return dot_product;
 }
 
-
-void vector_class_output()
+void vector_class_output(size_t option)
 {
+  if(option==0){
+      //Demonstrate default constructor
+      vector v1;
+      std::cout << "Default constructor (v1): " << std::endl << v1 << std::endl << dash(0) << std::endl;
+      //Demonstrate parameterized constructor
+      /* Make vector v2
+      |1 2|
+      |3 4|
+      */
+      vector v2{3};
+      std::cout << "Parameterized constructor (v2): " << std::endl << v2 << std::endl << dash(0) << std::endl;
+      v2.set_value(0,6); v2.set_value(1,3); v2.set_value(2,2);
+      std::cout << "Parameterized constructor (v2) after setting values (6,3,2): " << std::endl << v2 << std::endl << dash(0) << std::endl;
+      //Deep copy by assignment: define new vector v3 then copy from v2 to v3
+      std::cout << "Deep copy by assignmnent: " << std::endl;
+      vector v3{3};
+      std::cout << "Default (v3): " << std::endl << v3 << std::endl;
+      v3 = v2;
+      std::cout << "Deep copy (v2 to v3): " << std::endl << v3 << std::endl << dash(0) << std::endl;
+      //Modify contents of original vector and show assigned vector is unchanged here
+      std::cout << "Now modifying v2 and outputting v3: " << std::endl;
+      /* change v2 to
+      |2 2|
+      |2 2|
+      */
+      v2.set_value(0,2); v2.set_value(1,2); v2.set_value(2,2);
+      std::cout << "v2: " << std::endl << v2 << std::endl;
+      std::cout << "v3: " << std::endl << v3 << std::endl << dash(0) << std::endl;
+      //Deep copy using copy constructor
+      std::cout << "Deep copy using copy constructor (v2 to v4): " << std::endl;
+      vector v4{v2};
+      std::cout << "v4:" << std::endl << v4 << std::endl << dash(0) << std::endl;
+      //Modify contents of original vector and show copied vector is unchanged here
+      std::cout << "Now modifying v2 and outputting v4: " << std::endl;
+      /* change v2 to
+      |5 1|
+      |0 7|
+      */
+      v2.set_value(0,5); v2.set_value(1,1); v2.set_value(2,0);
+      std::cout << "v2:" << std::endl << v2 << std::endl;
+      std::cout << "v4:" << std::endl << v4 << std::endl << dash(0) << std::endl;
+      //Move copy construction demonstration
+      std::cout << "v5 creation using move constructor from v2: " << std::endl;
+      vector v5(std::move(v2));
+      std::cout << "v2:" << std::endl << v2;
+      std::cout << "v2 now empty, it has moved to v5 " << std::endl;
+      std::cout << "v5:" << std::endl << v5 << std::endl << dash(0) << std::endl;
+      // Move assignment demonstration
+      std::cout << "moving v5 back to v2 using move assignment: " << std::endl;
+      v2 = std::move(v5);
+      std::cout << "v2:" << std::endl << v2 << std::endl;
+      std::cout << "v5:" << std::endl << v5;
+      std::cout << "v5 now empty, has moved to v2 " << std::endl << dash(0) << std::endl;
+      std::cout << "Accessing 2nd value of vector v2 located at v2[1]: " << std::endl << v2[1] << std::endl << dash(0) << std::endl;
+      std::cout << "Now, enter two vectors to receive the dot product: " << std::endl;
+  }else if(option == 1){
+      std::cout << "Enter two vectors to receive the dot product: " <<  std::endl;
+  }
+  vector first_vector;
+  vector second_vector;
+  std::cin >> first_vector;
+  std::cout << dash(0) << std::endl;
+  std::cin >> second_vector;
+  std::cout << dash(0) << std::endl;
+  std::cout << "Your vectors: " << std::endl << "v1: "  << first_vector << std::endl;
+  std::cout << "v2: " << second_vector << std::endl << dash(0) << std::endl;
+  //Multiplication of 2 vectors
+  std::cout << "Dot product (v1*v2): " << std::endl << first_vector*second_vector << std::endl << dash(0) << std::endl;
+}
+
+//--------------------------------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------------------------------------------
+
+
+//4 Vector class in the form (ct,x,y,z)
+class four_vector : public vector
+{
+  //friends of the class
+  friend std::istream &operator>>(std::istream &in_stream, four_vector &a_four_vector);
+  friend std::ostream &operator<<(std::ostream &out_stream, const four_vector &a_four_vector); 
+
+public:
+  //Default, Parameterized, Copy, Move Constructor
+  four_vector();
+  four_vector(const double &ct, vector &a_vector);
+  four_vector(const four_vector &a_four_vector)  : vector(a_four_vector) {};
+  four_vector(four_vector &&a_four_vector) : vector(std::move(a_four_vector)) {};
+  //Copy and Move Assignment operators
+  four_vector &operator=(four_vector &a_four_vector);
+  four_vector &operator=(four_vector &&a_four_vector);
+  //Overload [] operator for accessing elements of the four vector
+  using vector::operator[];
+  //Dot Product for 4-vectors
+  double operator*(const four_vector& a_four_vector) const;
+  //Lorentz boost
+  four_vector lorentz_boost(vector &beta);
+};
+
+//Default Constructor
+four_vector::four_vector() : vector(4) {}
+//Parameterized Constructor with a double and a vector
+four_vector::four_vector(const double &ct, vector &a_vector) : vector(4) 
+{
+  vector_data[0] = ct;
+  for(size_t i{1}; i < 4; i++){
+        vector_data[i] = a_vector[i-1];
+  }
+}
+//Copy Assignment
+four_vector &four_vector::operator=(four_vector &a_four_vector)
+{
+  vector::operator=(a_four_vector);
+  return *this;
+}
+//Move Assignment 
+four_vector &four_vector::operator=(four_vector &&a_four_vector) 
+{
+  vector::operator=(std::move(a_four_vector));
+  return *this;
+}
+//Dot Product for 4-vectors
+double four_vector::operator*(const four_vector& a_four_vector) const
+{
+    double dot_product{};
+    if (size != 4) {
+        std::cout << "Error: vectors must have the same size" << std::endl; exit(1);
+    }
+    for (size_t i{1}; i < size; i++) {
+    dot_product += vector_data[i] * a_four_vector.vector_data[i];
+    }
+    return vector_data[0] * a_four_vector.vector_data[0] - dot_product;
+}
+
+
+std::istream& operator>>(std::istream& in_stream, four_vector &a_four_vector)
+{
+	/*
+	Input example:
+	1 2 12 2
+	*/
+  size_t size = 4;
+	//Get vector values 
+	std::cout << "Enter values of the four-vector (" << size << " values)" << std::endl;
+	//Loop to fill the vector from input
+	for (size_t i{}; i < size; i++) {
+        double temporary;
+        std::cin >> temporary;
+        std::cin.ignore();
+        if (std::cin.fail()) {
+            std::cout << "Error: invalid input" << std::endl; exit(1);   
+        }
+        a_four_vector.vector_data[i] = temporary;  
+	}
+	return in_stream;
+}
+
+std::ostream &operator<<(std::ostream &out_stream, const four_vector &a_four_vector)
+{
+  if(a_four_vector.get_size() > 0){
+    out_stream << "[";
+    for (size_t index{0}; index < a_four_vector.get_size(); index++) {
+      if (index == a_four_vector.get_size() - 1) {
+        out_stream << a_four_vector.vector_data[index] << "]";
+      }else{
+        out_stream << a_four_vector.vector_data[index] << ";";
+      }
+  }
+  }else{
+    out_stream << "vector is empty" << std::endl;
+  }
+  return out_stream;
+}
+
+void four_vector_class_output(size_t option)
+{
+  if(option==0){
     //Demonstrate default constructor
-    vector v1;
-    std::cout << "Default constructor (v1): " << std::endl << v1 << std::endl << dash(0) << std::endl;
-    //Demonstrate parameterized constructor
+    four_vector fv1;
+    std::cout << "Default constructor (fv1): " << std::endl << fv1 << std::endl << dash(0) << std::endl;
+    //Demonstrate parameterised constructor
     /* Make vector v2
     |1 2|
     |3 4|
     */
     vector v2{3};
-    std::cout << "Parameterized constructor (v2): " << std::endl << v2 << std::endl << dash(0) << std::endl;
-    v2.set_value(0,6); v2.set_value(1,3); v2.set_value(2,2);
-    std::cout << "Parameterized constructor (v2) after setting values (6,3,2): " << std::endl << v2 << std::endl << dash(0) << std::endl;
+    four_vector fv2{0,v2};
+    std::cout << "Parameterized constructor (fv2): " << std::endl << fv2 << std::endl << dash(0) << std::endl;
+    fv2[0] = 4;
+    fv2[1] = 6;
+    fv2[2] = 3;
+    fv2[3] = 2;
+    std::cout << "Parameterized constructor (fv2) after setting values (ct,x,y,z) to (4,6,3,2): " << std::endl << fv2 << std::endl << dash(0) << std::endl;
     //Deep copy by assignment: define new vector v3 then copy from v2 to v3
     std::cout << "Deep copy by assignmnent: " << std::endl;
-    vector v3{3};
-    std::cout << "Default (v3): " << std::endl << v3 << std::endl;
-    v3 = v2;
-    std::cout << "Deep copy (v2 to v3): " << std::endl << v3 << std::endl << dash(0) << std::endl;
+    four_vector fv3;
+    std::cout << "Default (fv3): " << std::endl << fv3 << std::endl;
+    fv3=fv2;
+    std::cout << "Deep copy (fv2 to fv3): " << std::endl << fv3 << std::endl << dash(0) << std::endl;
     //Modify contents of original vector and show assigned vector is unchanged here
-    std::cout << "Now modifying v2 and outputting v3: " << std::endl;
+    std::cout << "Now modifying fv2 and outputting fv3: " << std::endl;
     /* change v2 to
     |2 2|
     |2 2|
     */
-    v2.set_value(0,2); v2.set_value(1,2); v2.set_value(2,2);
-    std::cout << "v2: " << std::endl << v2 << std::endl;
-    std::cout << "v3: " << std::endl << v3 << std::endl << dash(0) << std::endl;
+    fv2[0] = 2;
+    fv2[1] = 2;
+    fv2[2] = 2;
+    fv2[3] = 2;
+    std::cout << "fv2: " << std::endl << fv2 << std::endl;
+    std::cout << "fv3: " << std::endl << fv3 << std::endl << dash(0) << std::endl;
     //Deep copy using copy constructor
-    std::cout << "Deep copy using copy constructor (v2 to v4): " << std::endl;
-    vector v4{v2};
-    std::cout << "v4:" << std::endl << v4 << std::endl << dash(0) << std::endl;
+    std::cout << "Deep copy using copy constructor (fv2 to fv4): " << std::endl;
+    four_vector fv4{fv2};
+    std::cout << "fv4:" << std::endl << fv4 << std::endl << dash(0) << std::endl;
     //Modify contents of original vector and show copied vector is unchanged here
-    std::cout << "Now modifying v2 and outputting v4: " << std::endl;
+    std::cout << "Now modifying fv2 and outputting fv4: " << std::endl;
     /* change v2 to
     |5 1|
     |0 7|
     */
-    v2.set_value(0,5); v2.set_value(1,1); v2.set_value(2,0);
-    std::cout << "v2:" << std::endl << v2 << std::endl;
-    std::cout << "v4:" << std::endl << v4 << std::endl << dash(0) << std::endl;
+    fv2[0] = 3;
+    fv2[1] = 5;
+    fv2[2] = 1;
+    fv2[3] = 0;
+    std::cout << "fv2:" << std::endl << fv2 << std::endl;
+    std::cout << "fv4:" << std::endl << fv4 << std::endl << dash(0) << std::endl;
     //Move copy construction demonstration
-    std::cout << "v5 creation using move constructor from v2: " << std::endl;
-    vector v5(std::move(v2));
-    std::cout << "v2:" << std::endl << v2;
-    std::cout << "v2 now empty, it has moved to v5 " << std::endl;
-    std::cout << "v5:" << std::endl << v5 << std::endl << dash(0) << std::endl;
+    std::cout << "fv5 creation using move constructor from fv2: " << std::endl;
+    four_vector fv5(std::move(fv2));
+    std::cout << "fv2:" << std::endl << fv2;
+    std::cout << "fv2 now empty, it has moved to fv5 " << std::endl;
+    std::cout << "fv5:" << std::endl << fv5 << std::endl << dash(0) << std::endl;
     // Move assignment demonstration
-    std::cout << "moving v5 back to v2 using move assignment: " << std::endl;
-    v2 = std::move(v5);
-    std::cout << "v2:" << std::endl << v2 << std::endl;
-    std::cout << "v5:" << std::endl << v5;
-    std::cout << "v5 now empty, has moved to v2 " << std::endl << dash(0) << std::endl;
-    
-
-    vector first_vector;
-    vector second_vector;
-    std::cin >> first_vector;
-    std::cout << dash(0) << std::endl;
-    std::cin >> second_vector;
-    std::cout << dash(0) << std::endl;
-    std::cout << "Your vectors: " << std::endl << "v1: " << std::endl << first_vector << std::endl;
-    std::cout << "v2: " << std::endl << second_vector << std::endl << dash(0) << std::endl;
-    //Multiplication of 2 vectors
-    std::cout << "Dot product (v1*v2): " << std::endl << first_vector*second_vector << std::endl << dash(0) << std::endl;
-    std::string option;
-    getline(std::cin, option);
+    std::cout << "moving fv5 back to fv2 using move assignment: " << std::endl;
+    fv2 = std::move(fv5);
+    std::cout << "fv2:" << std::endl << fv2 << std::endl;
+    std::cout << "fv5:" << std::endl << fv5;
+    std::cout << "fv5 now empty, has moved to fv2 " << std::endl << dash(0) << std::endl;
+    std::cout << "Accessing 2nd value of vector fv2 located at fv2[1]: " << std::endl << fv2[1] << std::endl << dash(0) << std::endl;
+    std::cout << "Now, enter two vectors to receive the dot product: " << std::endl;
+  }else if(option == 1){
+    std::cout << "Enter two four-vectors to receive the dot product: " <<  std::endl;
+  }
+  four_vector first_four_vector;
+  four_vector second_four_vector;
+  std::cin >> first_four_vector;
+  std::cout << dash(0) << std::endl;
+  std::cin >> second_four_vector;
+  std::cout << dash(0) << std::endl;
+  std::cout << "Your vectors: " << std::endl << "fv1: "  << first_four_vector << std::endl;
+  std::cout << "fv2: " << second_four_vector << std::endl << dash(0) << std::endl;
+  //Multiplication of 2 vectors
+  std::cout << "Dot product (fv1*fv2): " << std::endl << first_four_vector*second_four_vector << std::endl << dash(0) << std::endl;
 }
 
 int main()
 {
-    //Clears prvious inputs in the terminal
-    std::system("clear");
+  //Clears prvious inputs in the terminal
+  std::system("clear");
 	std::cout << std::setprecision(3);
 	std::cout << dash(0) << dash(0) << std::endl << "vector calculator" << std::endl << dash(0) << dash(0) << std::endl;
 	std::cout << "Which option would you like to choose?" << std::endl;
-	std::cout << "1 - vector class" << std::endl;
-	std::cout << "2 - 4-vector class" << std::endl;
-    std::cout << "3 - particle class" << std::endl;
-	std::cout << "4 - all" << std::endl << dash(0) << dash(0) << std::endl;
+	std::cout << "-1   vector class" << std::endl;
+  std::cout << "---1b   vector class dot product only" << std::endl;
+	std::cout << "-2   4-vector class" << std::endl;
+  std::cout << "---2b   4-vector class dot product only" << std::endl;
+  std::cout << "-3   particle class" << std::endl;
+	std::cout << "-4   all" << std::endl << dash(0) << dash(0) << std::endl;
 	//Asks user to choose an option
 	std::string option;
 	getline(std::cin, option);
-    if (option == "1") {
-		vector_class_output();
-	}
-    else if (option == "2") {}
+  if (option == "1") {
+		vector_class_output(0);
+	}else if (option == "1b"){
+    vector_class_output(1);
+  }else if (option == "2") {
+    four_vector_class_output(0);
+  }else if (option == "2b"){
+    four_vector_class_output(1);
+  }
         /*
 	}else if (option == "3") {
 		std::cout << dash() << std::endl;
@@ -296,7 +490,9 @@ int main()
 		std::cout << dash() << std::endl;
 	}
     */
-    
+  else{
+    std::cout << "No valid option detected " << std::endl;
+  }
 	std::cout << "Input anything to exit " << std::endl;
 	std::cin.ignore();
 	std::cin.get();
