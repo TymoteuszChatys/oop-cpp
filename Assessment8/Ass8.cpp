7be325e9216136d71d485c0c5d9a0cf287241e91
TymoteuszChatys <TymoteuszChatys@gmail.com>
2020-03-23 13:40:03 +0000

    base classes and derived classes

--- Assessment8/Ass8.cpp
+++ Assessment8/Ass8.cpp
@@ -5,9 +5,36 @@
 //Abstract base class with pure virtual functions and a virtual destructor.
 class shape
 {
+protected:
+    int dimensions; 
+    double *parameters; 
 public:
-	virtual ~shape(){}
-	virtual double area()=0;  //2D shapes
-	virtual double volume()=0;//3D shapes
+    shape(const int &dimension) {parameters = new double[dimensions];}
+    virtual ~shape() {delete[] parameters;};          
 };
 
+class two_dimensional : public shape
+{
+public:
+    virtual double area();
+    two_dimensional() : shape(2){};
+    two_dimensional(const double parameter_one, const double parameter_two);
+};
+
+class three_dimensional : public shape
+{
+public:
+    virtual double volume();
+    three_dimensional(): shape(3){};
+    three_dimensional(const double parameter_one, const double parameter_two, const double parameter_three);
+};
+
+class rectangle : public two_dimensional
+{
+
+};
+
+int main()
+{
+
+}
